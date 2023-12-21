54329420098d4d5582d3f635d62e26412e486200
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2023-12-21 11:02:21 +0100

    [code] Spring init

---
+++ src/main/java/it/unical/demacs/enterprise/fintedapp/FintedAppApplication.java
@@ -0,0 +1,14 @@
+package it.unical.demacs.enterprise.fintedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintedAppApplication.class, args);
+		
+	}
+
+}
