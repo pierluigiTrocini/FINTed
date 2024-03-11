ac494a12fd84c54df2134230c31f7cbd6e7ebb8d
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2024-03-11 15:55:27 +0100

    [code] pom.xml updated

--- FINTed_android/app/src/main/java/it/unical/demacs/enterprise/fintedapp/apis/ApiResources.kt
+++ FINTed_android/app/src/main/java/it/unical/demacs/enterprise/fintedapp/apis/ApiResources.kt
@@ -2,15 +2,4 @@ package it.unical.demacs.enterprise.fintedapp.apis
 
 data class ApiResources(
     val backendUrl: String = "http://192.168.1.117:8080",
-
-    val authUrl: String = "http://192.168.1.117:8090",
-
-    val authPath: String = "/realms/finted/protocol/openid-connect/token",
-    val logoutPath: String = "/realms/finted/protocol/openid-connect/logout",
-    val tokenIntrospection: String = "/realms/finted/protocol/openid-connect/token/introspect",
-    val grant_type_password: String = "password",
-    val grant_type_refresh: String = "refresh_token",
-    val client_id: String = "finted",
-    val client_secret: String = "dWqsIb2MFlmFhf5t0AuROGYbXnuTYuJc"
-
 )
