e8770bef3ea0606d35132efb3268f92d8abf2784
Pierluigi Trocini <59370899+pierluigiTrocini@users.noreply.github.com>
2024-04-22 10:31:03 +0200

    [frontend] Offer dialog implemented

--- FINTed_android/app/src/main/java/it/unical/demacs/enterprise/fintedapp/PostActivity.kt
+++ FINTed_android/app/src/main/java/it/unical/demacs/enterprise/fintedapp/PostActivity.kt
@@ -1,19 +1,27 @@
 package it.unical.demacs.enterprise.fintedapp
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import it.unical.demacs.enterprise.fintedapp.models.PostDto
 import it.unical.demacs.enterprise.fintedapp.viewmodels.OfferViewModel
 import it.unical.demacs.enterprise.fintedapp.viewmodels.PostViewModel
@@ -24,11 +32,17 @@ fun PostActivity(
     postViewModel: PostViewModel,
     offerViewModel: OfferViewModel
 ) {
+    val offerState = remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier.padding(16.dp),
         shape = RoundedCornerShape(8.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
             //TODO image
             Spacer(modifier = Modifier.height(8.dp))
             Text(
@@ -37,14 +51,51 @@ fun PostActivity(
             )
             Spacer(modifier = Modifier.height(4.dp))
             Text(
-                text = stringResource(id = R.string.postStartingPrice) + post.startingPrice ?: stringResource(id = R.string.unavailable),
+                text = stringResource(id = R.string.postStartingPrice) + post.startingPrice
+                    ?: stringResource(id = R.string.unavailable),
                 style = MaterialTheme.typography.bodySmall
             )
             Spacer(modifier = Modifier.height(8.dp))
 
-            Button(onClick = { /*TODO: Handle click*/ }) {
-                Text(text= stringResource(id = R.string.makeOffer))
+            Button(onClick = {
+                offerState.value = true
+            }) {
+                Text(text = stringResource(id = R.string.makeOffer))
             }
         }
     }
+
+    if (offerState.value) {
+        val offerText = remember { mutableStateOf("") }
+
+        Dialog(onDismissRequest = { offerState.value = false }) {
+            Card(
+                modifier = Modifier.padding(16.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Box(modifier = Modifier.padding(10.dp)){
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.makeOffer),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        OutlinedTextField(
+                            value = offerText.value,
+                            onValueChange = { v -> offerText.value = v },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            label = { Text(stringResource(id = R.string.makeOffer)) }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        Button(onClick = {
+                            offerState.value = false
+                        }) {
+                            Text(text = stringResource(id = R.string.publish))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
